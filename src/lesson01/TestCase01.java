955a4e224d58359a9d23d4c9453a9ab1439be8b3
waridibofa <waridiebare@gmail.com>
2024-08-15 06:47:57 -0400

    Added a new feature

---
+++ src/lesson01/TestCase01.java
@@ -0,0 +1,5 @@
+package lesson01;
+
+public class TestCase01 {
+
+}
--- src/utils/Constants.java
+++ src/utils/Constants.java
@@ -4,4 +4,9 @@ public class Constants {
 
 	public static final String CONFIGURATION_FILEPATH = System.getProperty("user.dir")	+ "/configs/configuration.properties";
 	
+	
+	
+	
+	
+	
 }
